6ef3cb39f3c808d3501321a705d78731ff1ff3af
Gopal Kumar <gopalkumar8220@gmail.com>
2024-08-05 23:47:11 +0530

    Add useLocation

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,16 +1,5 @@
-import { useContext, useEffect } from "react";
-import noteContext from "../context/notes/NoteContetx";
 const About = () => {
-  const a = useContext(noteContext);
-  useEffect(() => {
-    a.update();
-  }, [a]);
-
-  return (
-    <div>
-      This is about {a.state.name} and he is in {a.state.class}
-    </div>
-  );
+  return <div>This is about</div>;
 };
 
 export default About;
